03858ae3c7d7bacdafbb2db0ec30be632fa518b3
Camilo Cunha de Azevedo <30880723+Camilotk@users.noreply.github.com>
2017-10-13 15:20:55 -0300

    POOI
    Calcula um aumento sobre produto

--- Venda.java
+++ Venda.java
@@ -1,10 +1,10 @@
-//feito por Camilo Cunha, cÛd 555062
+//feito por Camilo Cunha, c√≥d 555062
 
 import javax.swing.JOptionPane;
 
 class Venda {
   public static void main (String args [])  {
-    String opc = JOptionPane.showInputDialog(null, "Digite o cÛdigo: \n\nCÛdigo   Aumento \n1             10% \n3             25% \n4             30% \n8             50%\n ");
+    String opc = JOptionPane.showInputDialog(null, "Digite o c√≥digo: \n\nC√≥digo   Aumento \n1             10% \n3             25% \n4             30% \n8             50%\n ");
     Double valor = Double.parseDouble(JOptionPane.showInputDialog(null, "Digite o valor do Produto:"));
     
     Double aumento = 0.00;
@@ -12,22 +12,22 @@ class Venda {
     switch (opc) {
       case "1":
         aumento = (valor*0.10);
-        JOptionPane.showMessageDialog(null, "O valor total apÛs o acrÈscimo de 10% È "+(valor+aumento));
+        JOptionPane.showMessageDialog(null, "O valor total ap√≥s o acr√©scimo de 10% √© "+(valor+aumento));
         break;
       case "3":
         aumento = (valor*0.25);
-        JOptionPane.showMessageDialog(null, "O valor total apÛs o acrÈscimo de 25% È "+(valor+aumento));
+        JOptionPane.showMessageDialog(null, "O valor total ap√≥s o acr√©scimo de 25% √© "+(valor+aumento));
         break;
       case "4":
         aumento = (valor*0.30);
-        JOptionPane.showMessageDialog(null, "O valor total apÛs o acrÈscimo de 30% È "+(valor+aumento));
+        JOptionPane.showMessageDialog(null, "O valor total ap√≥s o acr√©scimo de 30% √© "+(valor+aumento));
         break;
       case "8":
         aumento = (valor*0.50);
-        JOptionPane.showMessageDialog(null, "O valor total apÛs o acrÈscimo de 50% È "+(valor+aumento));
+        JOptionPane.showMessageDialog(null, "O valor total ap√≥s o acr√©scimo de 50% √© "+(valor+aumento));
         break;
       default:
-        JOptionPane.showMessageDialog(null, "CÛdigo n„o encontrado.");
+        JOptionPane.showMessageDialog(null, "C√≥digo n√£o encontrado.");
                                       
   }
  }
