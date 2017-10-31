fd052e74af9798333f7971f1a6637dda8b079e20
Camilo Cunha de Azevedo <30880723+Camilotk@users.noreply.github.com>
2017-10-31 12:52:44 -0200

    Rename ComparaStringJean.java to Lista 1/ComparaStringJean.java

--- Lista 1/ComparaStringJean.java
+++ Lista 1/ComparaStringJean.java
@@ -3,8 +3,8 @@ import java.util.*;
 /* Exercicio 11
  * Autor: Camilo Cunha de Azevedo
  * 
- *  O programa dever· ler o nome de uma pessoa, letra por letra, 
- * e informar se o nome digitado foi ìJeanî ou n„o.
+ *  O programa dever√° ler o nome de uma pessoa, letra por letra, 
+ * e informar se o nome digitado foi ‚ÄúJean‚Äù ou n√£o.
  */
 
 public class ComparaStringJean {
@@ -16,9 +16,9 @@ public class ComparaStringJean {
     s = s.toUpperCase();
     
     if (j.equals(s)) {
-      System.out.println("…");
+      System.out.println("√â");
     }else {
-      System.out.println("N„o");
+      System.out.println("N√£o");
     }
       
     }
